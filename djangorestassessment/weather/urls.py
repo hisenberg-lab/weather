821293d33f1cfc010d62c0dbb2b8e84eda5cc817
Chandu B L <chandu.b.l537@gmail.com>
2021-08-08 03:52:58 +0530

    added index page and fixed json parsing of api data

--- djangorestassessment/weather/urls.py
+++ djangorestassessment/weather/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # path('',views.index,name='index'),
+    path('',views.index,name='index'),
     path('weather_data/', views.weather_data_api,name='get_weather_by_city'),
 ]
--- djangorestassessment/weather/views.py
+++ djangorestassessment/weather/views.py
@@ -8,6 +8,24 @@ from rest_framework import status
 
 # Create your views here.
 
+def index(request):
+    if request.method == 'POST':
+        city = request.POST.get("city")
+        result = requests.get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+settings.API_KEY)
+        if result:
+            api_data = json.loads(result.text)
+            data = {"city": str(api_data["name"]),
+                    "coordinates": "Lon "+str(api_data["coord"]["lon"])+" "+"Lat "+str(api_data["coord"]["lat"]),
+                    "weather": str(api_data["weather"][0]["description"]),
+                    "temp": "temp "+str(api_data["main"]["temp"])+", "+"feels like "+str(api_data["main"]["feels_like"]),
+                    "humidity": str(api_data["main"]["humidity"]),
+                    }
+            # print(data)
+        else:
+            data={"error":1}
+        return render(request,"index.html",data)
+    return render(request,"index.html")
+
 @api_view()
 def weather_data_api(request):
     """
@@ -19,4 +37,4 @@ Returns json data of weather for the given city
     if not result:
         response_data = {'message':'Invalid City Name'}
         return Response(response_data, status=status.HTTP_404_NOT_FOUND)
-    return Response(result.text) 
+    return Response(result.json()) 
