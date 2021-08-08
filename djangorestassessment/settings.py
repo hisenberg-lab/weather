a45c8877a23e97a546915a6ad9ca2a960274b3d9
Chandu B L <chandu.b.l537@gmail.com>
2021-08-08 08:11:23 +0530

    removed white noise

--- djangorestassessment/settings.py
+++ djangorestassessment/settings.py
@@ -54,8 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
 ]
 
 ROOT_URLCONF = 'djangorestassessment.urls'
